8707029bd08cd708fc6eaf9b1003e13705dc5fe8
Kevin Siwoff <info@kevinsiwoff.com>
2016-09-21 14:21:12 -0400

    adds comment to #46

--- week1-assignment/assignment1-1-#46green random angle.js
+++ week1-assignment/assignment1-1-#46green random angle.js
@@ -4,7 +4,8 @@ function setup() {
 
 function draw(){
   background(255);
-  
+  // while technically correct, consider add assigning the value of x in your loop header
+  // like this: for(var x=0; x <=600; x+=6)
   for(var x=0;x<=600;x=x+6){
     stroke(110,210,75);
     line(x,0,x,400);
@@ -16,6 +17,8 @@ function draw(){
     
     stroke(255);
     line(x,y+1,x,y+5);
+    //consider moving all of this code into your setup function and out of draw.
+    // you won't need to call noLoop() then.
     noLoop();
   }
 
